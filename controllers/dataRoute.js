c5d9a10930bf1c1fe7e7b8fd889a9ad9f9feca3d
ShivS01 <43378694+ShivS01@users.noreply.github.com>
2020-07-21 22:06:08 +0530

    defining get functions for all univ collections

--- controllers/dataRoute.js
+++ controllers/dataRoute.js
@@ -5,8 +5,6 @@ const db = require("../models/index");
 
 //fetches all data
 
-// const baserUrl = "https://just-pdf.herokuapp.com/";
-
 dataRouter.get("/", (request, response) => {
   Data.find({}).then((data) =>
     response.json(
@@ -34,40 +32,79 @@ dataRouter.get("/all", (request, response) => {
     });
 });
 
-dataRouter.get("/:univ/:school", async (request, response) => {
-  // db.University.find({ university: request.params.univ }).then(
-  // db.University.findOne({ abbv: request.params.univ })
-  //   .then()
-
-  //   .then(
-  //     (data) => response.json(data.map((dat) => dat.toJSON()))
-  //     // )
-  //   );
-
-  // const c = await getUniv();
-
-  getUniv = (univ) => {
-    return db.University.findOne({ abbv: univ })
-      .then((dbUniversity) => {
-        return dbUniversity.school.map((dat) => dat.toJSON());
-      })
-      .catch((err) => console.log(err));
-  };
-
-  getSchool = (school, univData) => {
-    return db.School.findOne({
-      $and: [{ abbv: school }, { abbv: univData }],
+getUniv = (univ) => {
+  return db.University.findOne({ abbv: univ })
+    .then((dbUniversity) => {
+      return dbUniversity.school.map((dat) => dat.toJSON());
     })
-      .then((dbSchool) => {
-        return dbSchool.school.map((dat) => dat.toJSON());
-      })
-      .catch((err) => console.log(err));
-  };
+    .catch((err) => console.log(err));
+};
 
+getSchool = (school, univData) => {
+  return db.School.findOne({
+    $and: [{ abbv: school }, { abbv: univData }],
+  })
+    .then((dbSchool) => {
+      return dbSchool.branch.map((dat) => dat.toJSON());
+    })
+    .catch((err) => console.log(err));
+};
+
+getBranch = (branch, schoolData) => {
+  return db.Branch.findOne({
+    $and: [{ abbv: branch }, { abbv: schoolData }],
+  })
+    .then((dbBranch) => {
+      return dbBranch.semesters.map((dat) => dat.toJSON());
+    })
+    .catch((err) => console.log(err));
+};
+
+getSemester = (semester, branchData) => {
+  return db.Semester.findOne({
+    $and: [{ semester }, { semester: branchData }],
+  })
+    .then((dbSemester) => {
+      return dbSemester.subject.map((dat) => dat.toJSON());
+    })
+    .catch((err) => console.log(err));
+};
+
+// change to search by id for book
+
+getSubject = (subject, semesterData) => {
+  return db.Subject.findOne({
+    $and: [{ abbv: subject }, { abbv: semesterData }],
+  })
+    .then((dbSubject) => {
+      return dbSubject.books.map((dat) => dat.toJSON());
+    })
+    .catch((err) => console.log(err));
+};
+
+getBook = (book, subjectData) => {
+  return db.Book.findOne({
+    $and: [{ title: book }, { title: subjectData }],
+  })
+    .then((dbBook) => {
+      return dbBook.Book.map((dat) => dat.toJSON());
+    })
+    .catch((err) => console.log(err));
+};
+
+dataRouter.get("/:univ", async (request, response) => {
   const university = await getUniv(request.params.univ);
-  // console.log(university);
+  response.json(university);
+});
+
+dataRouter.get("/:univ/:school", async (request, response) => {
+  const university = await getUniv(request.params.univ);
   const school = await getSchool(request.params.school, university);
-  console.log(school);
+  // const branch = await getBranch(request.params.branch, school);
+  // const semester = await getSemester(request.params.sem, branch);
+  // const subject = await getSubject(request.params.subject, semester);
+
+  response.json(school);
 });
 
 module.exports = dataRouter;
